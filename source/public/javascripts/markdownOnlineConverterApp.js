dab26ebae1ed5c9c0844c68220a55314fcc1efe6
Miguel Ferreira <mferreira@keep.pt>
2017-09-17 03:11:13 +0100

    updated code

--- source/public/javascripts/markdownOnlineConverterApp.js
+++ source/public/javascripts/markdownOnlineConverterApp.js
@@ -1,150 +1,109 @@
 
-var app = angular.module("markdownOnlineConverterApp", ["angularFileUpload"]);
+var app = angular.module("markdownOnlineConverterApp", ["angularFileUpload", 'ngSanitize']);
 
 
-app.controller("markdownOnlineConverterController", ['$scope', 'FileUploader', '$window', function($scope, FileUploader, $window) {
+app.controller("markdownOnlineConverterController", ['$scope', 'FileUploader', '$http', '$sce',
+    function($scope, FileUploader, $http, $sce) {
 
-        var uploader = $scope.uploader = new FileUploader({
-            url: '/upload'
-        });
-
-
+ $scope.upload = {};
+ $scope.upload.status = "onStart";
 
 
-        // FILTERS
-
-        // a sync filter
-        uploader.filters.push({
-            name: 'syncFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options) {
-                console.log('syncFilter');
-                return this.queue.length <= 1;
-            }
-        });
+  var uploader = $scope.uploader = new FileUploader({
+      url: '/convert'
+  });
 
 
-        // an async filter
-        uploader.filters.push({
-            name: 'asyncFilter',
-            fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
-                console.log('asyncFilter');
-                setTimeout(deferred.resolve, 1e3);
-            }
-        });
+  // a sync filter
+  uploader.filters.push({
+      name: 'syncFilter',
+      fn: function(item /*{File|FileLikeObject}*/, options) {
+          console.log('syncFilter');
+          return this.queue.length <= 1;
+      }
+  });
 
-        // CALLBACKS
-
-        uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
-            console.info('onWhenAddingFileFailed', item, filter, options);
-        };
-        uploader.onAfterAddingFile = function(fileItem) {
-            console.info('onAfterAddingFile', fileItem);
-            fileItem.upload();
-        };
-        uploader.onAfterAddingAll = function(addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
-        uploader.onBeforeUploadItem = function(item) {
-            console.info('onBeforeUploadItem', item);
-        };
-        uploader.onProgressItem = function(fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function(progress) {
-            console.info('onProgressAll', progress);
-        };
-        uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-        uploader.onErrorItem = function(fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function(fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteItem = function(fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
-            //$window.location.href = "/convert/" + response.fileId;
-            $scope.convertedUrl = "/convert/" + response.fileId;
-        };
-        uploader.onCompleteAll = function() {
-            console.info('onCompleteAll');
-            $scope.done = true;
-        };
-
-        console.info('uploader', uploader);
-    }]);
+  // an async filter
+  uploader.filters.push({
+      name: 'asyncFilter',
+      fn: function(item /*{File|FileLikeObject}*/, options, deferred) {
+          console.log('asyncFilter');
+          setTimeout(deferred.resolve, 1e3);
+      }
+  });
 
 
+  // CALLBACKS
+  uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+      //console.info('onWhenAddingFileFailed', item, filter, options);
+  };
+  uploader.onAfterAddingFile = function(fileItem) {
+      //console.info('onAfterAddingFile', fileItem);
+      fileItem.upload();
+  };
+  uploader.onAfterAddingAll = function(addedFileItems) {
+      //console.info('onAfterAddingAll', addedFileItems);
+  };
+  uploader.onBeforeUploadItem = function(item) {
+      //console.info('onBeforeUploadItem', item);
+  };
+  uploader.onProgressItem = function(fileItem, progress) {
+      //console.info('onProgressItem', fileItem, progress);
+      $scope.upload.status = "onProgressItem";
+  };
+  uploader.onProgressAll = function(progress) {
+      //console.info('onProgressAll', progress);
+  };
+  uploader.onSuccessItem = function(fileItem, response, status, headers) {
+      //console.info('onSuccessItem', fileItem, response, status, headers);
+  };
+  uploader.onErrorItem = function(fileItem, response, status, headers) {
+      //console.info('onErrorItem', fileItem, response, status, headers);
+  };
+  uploader.onCancelItem = function(fileItem, response, status, headers) {
+      //console.info('onCancelItem', fileItem, response, status, headers);
+  };
+  uploader.onCompleteItem = function(fileItem, response, status, headers) {
+      console.info('onCompleteItem', fileItem, response, status, headers);
+      //$window.location.href = "/convert/" + response.fileId;
+      //$scope.convertedUrl = "/convert/" + response.fileId;
+
+      $scope.upload.status = "onCompleteItem";
+      $scope.pdf = "/" + response.id;
+
+      //$scope.pdf = "http://www.google.com";
+      // convertToPdf(response.fileId, function(err, data) {
+      //   //$scope.pdfAsBase64 = "data:application/pdf;base64," + btoa(unescape(encodeURIComponent(data)));
+      //   $scope.pdfAsBase64 = "data:application/pdf;base64," + btoa(unescape(encodeURIComponent(data)));
+      //
+      // });
+  };
+
+  uploader.onCompleteAll = function() {
+    console.info('onCompleteAll');
+    //$scope.upload.status = "onCompleteAll";
+  };
 
 
-//
-// app.config(function($routeProvider) {
-//   $routeProvider
-//   .when("/", {
-//     templateUrl : "form.html",
-//     controller: "chatController"
-//   })
-//   .when("/messages", {
-//     templateUrl : "messages.html",
-//     controller: "chatController"
-//   });
-// });
-//
 
-//
-// app.controller("markdownOnlineConverterController", function ($scope, $http, $interval, $location) {
-//     $scope.name = "Miguel";
-//     $scope.message = "";
-//     $scope.messages = [];
-//     $scope.$location = $location;
-//
-//
-//     function updateMessages() {
-//         $http.get('/messages').then(function(response) {
-//             $scope.messages = response.data;
-//         }, function(error) {
-//             // do nothing
-//         }
-//         );
-//     }
-//
-//
-//     updateMessages();
-//
-//     // install timer to update messages every second
-//     //$interval(updateMessages, 1000);
-//
-//
-//
-//     $scope.deleteMessage = function(id) {
-//         $http.delete('/messages/' + id).then(function(response) {
-//             $scope.messages = response.data;
-//
-//             //redirect to messages route
-//             $location.url("/messages");
-//         }, function(error) {
-//             // do nothing
-//             console.log(error);
-//         }
-//         );
-//     }
-//
-//     $scope.sendMessage = function() {
-//         var data = {
-//             'name': $scope.name,
-//             'message': $scope.message,
-//         }
-//
-//         $http.post('/messages', data).then(function(response) {
-//             $scope.messages = response.data;
-//
-//             //redirect to messages route
-//             $location.url("/messages");
-//         }, function(error) {
-//             // do nothing
-//             console.log(error);
-//         }
-//         );
-//     }
-// });
+  $scope.loadPage = function() {
+    $scope.pdf = "http://localhost:3000/314d145299e2dec6972360e2b94971e9.pdf";
+  }
+
+
+  $scope.trustSrc = function(src) {
+    return $sce.trustAsResourceUrl(src);
+  }
+
+  //console.info('uploader', uploader);
+
+
+  convertToPdf = function(fileId, callback) {
+     $http.get("/convert/" + fileId).then(function(response) {
+       console.dir(response);
+       callback(null, response.data);
+     }, function(error) {
+       callback(error, null);
+     });
+  }
+}]);
--- source/routes/index.js
+++ source/routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer');
-var upload = multer({ dest: './uploads/' });
+var upload = multer({ dest: './filestore/' });
 var markdownpdf = require("markdown-pdf");
 var fs = require("fs");
-
+var base64 = require('base64-stream');
+var path = require('path');
 
 
 
@@ -14,27 +15,65 @@ router.post('/upload', upload.single('file'), function(req, res, next) {
   var data = {
     'message': 'File upload complete',
     'fileId': req.file.filename,
-    // 'size': req.file.size
+    'size': req.file.size
   };
 
 	res.jsonp(data);
 });
 
 
-// Converts to MD to PDF
-router.get('/convert/:fileId', function(req, res, next) {
+
+
+router.post('/convert', upload.single('file'), function(req, res, next) {
+	console.dir(req.file);
+
+  var data = {
+    'id': req.file.filename + '.pdf',
+  };
+
+  var markdownFilepath = req.file.path;
+  var pdfFilePath = path.join(__dirname, '../filestore/' + req.file.filename + '.pdf');
+
+  var pdfWriteStream = fs.createWriteStream(pdfFilePath);
+  convertToPdf(markdownFilepath, pdfWriteStream, true, function() {
+    console.log("PDF written at " + pdfFilePath);
+    res.jsonp(data);
+  });
+
+});
+
+
+
+
+
+function convertToPdf(markdownFilepath, writeStream, deleteinputFile, callback) {
 
   var options = {
     //cssPath: "./KEEPS.css"
   }
 
-  var filename = "uploads/" + req.params.fileId;
+  var stream = fs.createReadStream(markdownFilepath)
+                .pipe(markdownpdf(options))
+                .pipe(writeStream);
 
-  fs.createReadStream(filename)
-    .pipe(markdownpdf(options))
-    .pipe(res);
-  //fs.unlinkSync(filename);
-});
+
+    stream.on('finish', function () {
+      if(deleteinputFile) {
+        fs.unlink(markdownFilepath, callback);
+      } else {
+        callback();
+      }
+    });
+
+}
+
+// // Converts to MD to PDF
+// router.get('/:filename', function(req, res, next) {
+//   var filePath =  path.join(__dirname, '../filestore/' + req.params.filename);
+// 	//res.type('application/pdf')
+// 	//res.setHeader
+//   res.sendFile(filePath);
+// });
 
 
 module.exports = router;
